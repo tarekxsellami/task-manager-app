08793446aa136e96be697752e58c3a8521752987
tarek <admin@socialcode.club>
2024-11-08 16:34:20 +0100

    init classes

---
+++ app/src/main/java/com/socialcode/taskmanager/ui/addtask/AddTaskScreen.kt
@@ -0,0 +1,15 @@
+package com.socialcode.taskmanager.ui.addtask
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun AddTaskScreen() {
+    Scaffold {
+        Column(modifier = Modifier.padding(it)) { }
+    }
+}
---
+++ app/src/main/java/com/socialcode/taskmanager/ui/addtask/AddTaskViewModel.kt
@@ -0,0 +1,10 @@
+package com.socialcode.taskmanager.ui.addtask
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class AddTaskViewModel @Inject constructor() : ViewModel() {
+}
