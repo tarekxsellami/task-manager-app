a626157b05d76447df174c360642d73b033346da
tarek <admin@socialcode.club>
2024-11-08 16:04:16 +0100

    remove docs

--- app/src/main/java/com/socialcode/taskmanager/TaskManagerApplication.kt
+++ app/src/main/java/com/socialcode/taskmanager/TaskManagerApplication.kt
@@ -4,11 +4,5 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 
-/**
- * Created by Tarek Sellami (tarek@evenly.io) on 08.11.2024
- *
- * Copyright (c) 2024 Evenly GmbH,
- * all rights reserved
- */
 @HiltAndroidApp
 class TaskManagerApplication : Application()
--- app/src/main/java/com/socialcode/taskmanager/core/database/AppDatabase.kt
+++ app/src/main/java/com/socialcode/taskmanager/core/database/AppDatabase.kt
@@ -5,12 +5,6 @@ import androidx.room.RoomDatabase
 import com.socialcode.taskmanager.core.data.Task
 
 
-/**
- * Created by Tarek Sellami (tarek@evenly.io) on 08.11.2024
- *
- * Copyright (c) 2024 Evenly GmbH,
- * all rights reserved
- */
 @Database(entities = [Task::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 }
